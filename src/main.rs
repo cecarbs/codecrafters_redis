be854562d7570c191c44ad0f03d9d9c92103815f
Charles Carbonel <carbonel257@gmail.com>
2024-03-14 19:06:01 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let cli_args = HelperCLI::new(args);
     let role = cli_args.role;
-
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind(&cli_args.port).await?;
 
     println!("Server started on port: {}", cli_args.port);
 
-    let role = match &role {
-        helper_cli::Role::Master(master_role) => master_role,
-        helper_cli::Role::Slave(slave_role) => slave_role,
-    };
-
-    println!("Role is: {}", role);
-
     loop {
         match listener.accept().await {
             Ok((socket, _)) => {
+                let role = match &role {
+                    helper_cli::Role::Master(master_role) => master_role,
+                    helper_cli::Role::Slave(slave_role) => slave_role,
+                };
+
+                println!("Role is: {}", role);
                 println!("Established connection with client.");
 
                 tokio::spawn(handle_connection(socket, role.to_owned()));
